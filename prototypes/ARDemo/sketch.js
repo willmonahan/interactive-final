398bbed32979fb7184b5323eee93454fa29a563f
willmonahan <willmonahan13@gmail.com>
2017-12-04 13:38:47 -0500

    added AR driving
    the car model (from Google Poly) has trouble loading the texture, but
    the model works, and it drives using the driving physics from the
    driving demo

--- prototypes/ARDemo/sketch.js
+++ prototypes/ARDemo/sketch.js
@@ -4,33 +4,83 @@ var world;
 // create a variable to hold our marker
 var marker;
 
+var car;
+
 function setup() {
-  // create our world (this also creates a p5 canvas for us)
-  world = new World('ARScene');
+	// create our world (this also creates a p5 canvas for us)
+	world = new World('ARScene');
+	angleMode(DEGREES);
 
-  // grab a reference to the marker that we set up on the HTML side (connect to it using its 'id')
-  marker = world.getMarker('w_marker');
+	// grab a reference to the marker that we set up on the HTML side (connect to it using its 'id')
+	marker = world.getMarker('w_marker');
 
-  // create some geometry to add to our marker
-  var littleCube = new Box({
-    x:0,
-    y:0.25,
-    z:0,
-    red:0,
-    green:255,
-    blue:0,
-    width:0.5,
-    height:0.5,
-    depth:0.5
-  });
+	// create some geometry to add to our marker
+	/*var littleCube = new Box({
+		x:0,
+		y:0.25,
+		z:0,
+		red:0,
+		green:255,
+		blue:0,
+		width:0.5,
+		height:0.5,
+		depth:0.5
+	});*/
 
-  // add the cube to our marker
-  marker.addChild(littleCube);
+	// add the cube to our marker
+	//marker.addChild(littleCube);
+
+	car = new Car();
+	marker.addChild(car.object);
 }
 
 
 function draw() {
+	car.move();
+}
 
+function Car() {
+	this.object = new OBJ({
+		asset: 'car_obj',
+		mtl: 'car_mtl',
+		x: 0,
+		y: 0.5,
+		z: 0,
+		scaleX:0.003,
+		scaleY:0.003,
+		scaleZ:0.003,
+	});
 
+	this.speed = 0;
+	this.max = 0.07;
+	this.acc = 0.005;
+	this.dec = 0.006;
+	this.dir = 0;
+	this.object.rotateY(this.dir);
+	this.turn = 8;
 
+	this.move = function() {
+		if (keyIsDown(38)) { //up arrow
+			this.speed += this.acc;
+		} else {
+			this.speed -= this.dec;
+		}
+		this.speed = constrain(this.speed, 0, this.max);
+
+		if (this.speed > 0) {
+			if (keyIsDown(37)) { //left arrow
+				this.dir += this.turn*(this.speed/this.max);
+
+			}
+
+			if (keyIsDown(39)) { //right arrow
+				this.dir -= this.turn*(this.speed/this.max);
+
+			}
+			this.object.rotateY(this.dir);
+		}
+
+		this.object.nudge((this.speed*sin(this.dir)),0,(this.speed*cos(this.dir)));
+		//console.log(this.dir);
+	}
 }
