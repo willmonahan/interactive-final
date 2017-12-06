f78a37d923b8089b23effb57b01037d1a2054ec2
willmonahan <willmonahan13@gmail.com>
2017-12-05 23:59:34 -0500

    add window resizing

--- prototypes/ARFirebaseDemo/controller/sketch.js
+++ prototypes/ARFirebaseDemo/controller/sketch.js
@@ -13,7 +13,19 @@ function setup() {
 	fill(2*255/5);
 	rect(width/2,0,width/2,height/2)
 	fill(255/5);
-	rect(width/2,height/2,width/2,height/2)
+	rect(width/2,height/2,width/2,height/2);
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
+	fill(4*255/5); //turn left, green
+	rect(0,0,width/4,height);
+	fill(3*255/5); //turn right, blue
+	rect(width/4,0,width/4,height);
+	fill(2*255/5);
+	rect(width/2,0,width/2,height/2)
+	fill(255/5);
+	rect(width/2,height/2,width/2,height/2);
 }
 
 function draw() {
