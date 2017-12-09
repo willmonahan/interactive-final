12df934a6969b91cefb69c75fce8007bb73ebd00
willmonahan <willmonahan13@gmail.com>
2017-12-09 17:18:34 -0500

    copy the AR Firebase Demo to be the basis for the real game

---
+++ prototypes/game/controller/sketch.js
@@ -0,0 +1,102 @@
+var command = "none";
+var canvas;
+var newCommands = "";
+var prevCommands = "";
+function setup() {
+	canvas = createCanvas(windowWidth,windowHeight);
+
+	canvas.parent("#controller");
+	noStroke();
+
+	fill(4*255/5); //turn left, green
+	rect(0,0,width/4,height);
+	fill(3*255/5); //turn right, blue
+	rect(width/4,0,width/4,height);
+	fill(2*255/5);
+	rect(width/2,0,width/2,height/2)
+	fill(255/5);
+	rect(width/2,height/2,width/2,height/2);
+}
+
+function windowResized1() {
+	resizeCanvas(windowWidth, windowHeight);
+	fill(4*255/5); //turn left, green
+	rect(0,0,width/4,height);
+	fill(3*255/5); //turn right, blue
+	rect(width/4,0,width/4,height);
+	fill(2*255/5);
+	rect(width/2,0,width/2,height/2)
+	fill(255/5);
+	rect(width/2,height/2,width/2,height/2);
+}
+
+function draw() {
+	if (user_info) {
+		var turnCommand = "none";
+		var isTurning = false;
+		var goCommand = "none";
+		var isGoing = false;
+		for (var i = 0; i < touches.length; i++) {
+			if (touches[i].x < width/4) {
+				turnCommand = "left";
+				isTurning = true;
+			} else if (touches[i].x > width/4 && touches[i].x < width/2) {
+				turnCommand = "right";
+				isTurning = true;
+			} else {
+				if (touches[i].y < height/2) {
+					goCommand = "forward"
+					isGoing = true;
+				} else if(touches[i].y > height/2) {
+					goCommand = "backward"
+					isGoing = true;
+				}
+			}
+		}
+		newCommands = turnCommand + goCommand;
+
+		if (newCommands != prevCommands) {
+			var userData = {
+	 			goAction: goCommand,
+				turnAction: turnCommand
+	 		};
+
+	 		var update = {};
+	 		update[user_info.uid+"/"] = userData;
+	 		firebase.database().ref().update(update);
+			prevCommands = newCommands;
+		}
+	}
+}
+
+// function mousePressed() {
+// 	if (mouseX < width/2) {
+// 		command = "left";
+// 	} else {
+// 		command = "right";
+// 	}
+//
+// 	if (user_info) {
+// 		var userData = {
+// 			action: command
+// 		};
+//
+// 		var update = {};
+// 		update[user_info.uid+"/"] = userData;
+// 		firebase.database().ref().update(update);
+// 	}
+// }
+//
+// function mouseReleased() {
+// 	command = "none";
+//
+// 	if (user_info) {
+// 		var userData = {
+// 			action: command
+// 		};
+//
+// 		var update = {};
+// 		update[user_info.uid+"/"] = userData;
+// 		firebase.database().ref().update(update);
+// 	}
+// }
---
+++ prototypes/game/sketch.js
@@ -0,0 +1,167 @@
+// create a variable to hold our world object
+var world;
+
+// create a variable to hold our marker
+var marker;
+
+var allCars = {};
+var allData;
+
+function setup() {
+	world = new World('ARScene');
+	angleMode(DEGREES);
+
+	marker = world.getMarker('w_marker');
+
+	//marker.addChild(car.object);
+
+	var updates = firebase.database().ref("/");
+	updates.on("value", function(snapshot) {
+		allData = snapshot.val();
+
+		if (allData != null) {
+			//console.log("Database Updated");
+			for (var key in allData) {
+				if (typeof allCars[key] == "undefined" && key != "undefined") {
+					allCars[key] = new Car();
+					marker.addChild(allCars[key].object);
+				}
+			}
+
+			// remove any old unused cars
+			for (var key in allCars) {
+				if (typeof allData[key] == "undefined") {
+					console.log(allCars);
+					marker.removeChild(allCars[key].object);
+					delete allCars[key];
+					console.log(allCars);
+				}
+			}
+		}
+		else {
+			for (var key in allCars) {
+				//console.log(allCars);
+				marker.removeChild(allCars[key].object);
+				delete allCars[key];
+				//console.log(allCars);
+			}
+		}
+
+	})
+
+}
+
+
+function draw() {
+
+	//console.log(allCars);
+
+	//console.log(allData);
+	if (allData != null) {
+		for (var key in allCars) {
+			if (typeof allData[key] != "undefined") {
+				if (allData[key].turnAction == "left") {
+					allCars[key].turnLeft();
+				}
+				if (allData[key].turnAction == "right") {
+					allCars[key].turnRight();
+				}
+				if (allData[key].goAction == "forward") {
+					allCars[key].goForwards();
+				} else if (allData[key].goAction == "backward") {
+					allCars[key].goBackwards();
+				} else if (allCars[key].speed > allCars[key].acc/2 || allCars[key].speed < -allCars[key].acc/2) {
+					allCars[key].decelerate();
+					//console.log("beep");
+				}
+			}
+		}
+
+
+		for (var key in allCars) {
+			allCars[key].move();
+		}
+	}
+}
+
+function Car() {
+	this.object = new OBJ({
+		asset: 'car_obj',
+		mtl: 'car_mtl',
+		x: 0,
+		y: 0,
+		z: 0,
+		scaleX:0.008,
+		scaleY:0.008,
+		scaleZ:0.008,
+	});
+
+	this.speed = 0;
+	this.max = 0.07;
+	this.acc = 0.003;
+	this.dec = 0.003;
+	this.dir = 0;
+	this.moving = "forwards"
+	this.object.rotateY(this.dir);
+	this.turn = 5;
+	this.go = false;
+
+	this.move = function() {
+		//console.log(this.object.getX() + " - " + this.object.getZ());
+		if (this.speed > 0) {
+			this.speed = constrain(this.speed, 0, this.max);
+		} else if (this.speed < 0) {
+			this.speed = constrain(this.speed, -this.max, 0);
+		}
+
+		if (this.speed < this.acc/2 && this.speed > -this.acc/2) {
+			this.speed = 0;
+		} else {
+			this.object.rotateY(this.dir);
+		}
+
+		this.object.nudge((this.speed*sin(this.dir)),0,(this.speed*cos(this.dir)));
+
+		//console.log(this.dir);
+
+		if (this.object.getZ() > 8) {
+			this.object.setZ(-8);
+		}
+		if (this.object.getZ() < -8) {
+			this.object.setZ(8);
+		}
+		if (this.object.getX() > 8) {
+			this.object.setX(-8);
+		}
+		if (this.object.getX() < -8) {
+			this.object.setX(8);
+		}
+	}
+
+	this.goForwards = function() {
+		this.speed += this.acc;
+		this.moving = "forwards";
+	}
+
+	this.goBackwards = function() {
+		this.speed -= this.acc;
+		this.moving = "backwards";
+	}
+
+	this.decelerate = function() {
+		//this.speed -= this.dec;
+		if (this.speed > 0) {
+			this.speed -= this.dec;
+		} else {
+			this.speed += this.dec;
+		}
+	}
+
+	this.turnLeft = function() {
+		this.dir += this.turn*(this.speed/this.max);
+	}
+
+	this.turnRight = function() {
+		this.dir -= this.turn*(this.speed/this.max);
+	}
+}
